f4da57c292c8e039c160e827a1c640f6011daab3
Shoxrux0911 <usmonovvshohruhh@gmail.com>
2025-02-27 14:51:17 +0500

    Create boolen.py

---
+++ lesson-5/homework/boolen.py
@@ -0,0 +1,50 @@
+
+# year = int(input("Write a year: " ))
+# if (year % 4 == 0 and year % 100 !=0) or (year % 100 == 0):
+#     print("yes it is leap year")
+# else:
+#     print("No it is not") 
+
+# year = int(input("Enter your age: " ))
+
+# if year > 18:
+#     print("You can drive")
+# elif year < 18:
+#     b= 18 - year
+#     print(f"You need {b} more year")
+
+# a= int(input("enter 1st number: "))
+# b= int(input("enter 2nd number: "))
+
+# if a > b:
+#     print(f"{a} is greater than {b}")
+# elif a < b:
+#     print(f"{a} is less than {b}")
+# else:
+#     print(f"{a} equla {b}")
+
+# a = input("Write a month of the year: ").lower()
+
+
+# if a in ("september", "october", "november"):
+#     print("Autmn")
+# elif a in ("march", "april", "may"):
+#     print("Spring")
+# elif a in ("june", "july", "august"):
+#     print("Summer")
+# elif a in ("december", "january", "february"):
+#     print("Winter")
+# else:
+#     print("No availeble this month")
+
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+
+# new_fruit = "banana"
+
+# if new_fruit in fruits:
+#     print("this fruit is available")
+# else:
+#     fruits.append(new_fruit)
+#     print("modified version: ", fruits)
+ 
+
