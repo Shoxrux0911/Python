82f9e472f87c9ca9a3adce6192ae2a6cfb8046d4
Shoxrux0911 <usmonovvshohruhh@gmail.com>
2025-02-27 14:52:26 +0500

    Create forloop.py

---
+++ lesson-6/homework/forloop.py
@@ -0,0 +1,135 @@
+# for i in range(1, 11):
+#     print(i)
+
+# for i in range(1, 6):
+#     print("".join(i))
+
+
+# for i in range(1, 6):  # Loop from 1 to 5
+#     for j in range(1, i + 1):  # Loop to print numbers in each row
+#         print(j, end=" ")  # Print numbers on the same line
+#     print()  # Move to the next line
+
+# number = int(input("enter number: "))
+# total_number = 0
+# for i in range(1, number+1):
+#     total_number +=i
+# print(total_number)
+
+# n = 1
+# while n <=3:
+#     print(n)
+#     n+=1
+
+# number = int(input("enter number: "))
+# sum = 0
+
+# while number > 0:
+#     sum+=number
+#     number -=1
+# print(sum)
+
+# number = int(input("enter number: "))
+# count = 0
+# while number > 0:
+#     count+=1
+#     number//=10
+# print(count)
+
+# n = [1, 2, 3, 4, 5]
+
+# for i in (n[::-1]):
+#     print(i)
+
+# for i in range(-10, 0):
+#     print(i)
+
+
+# for i in range(10):
+#     print(i)
+# print("Done")
+
+# for i in range(1,10):
+#     print(i)
+
+# n= range(10)
+
+# while n>0:
+#     n-=1
+#     print(n)
+
+# n = 0
+
+# while n <= 10 :
+#     print(n)
+#     n+=1
+
+# for i in range(8):
+#     print("# "*8)
+
+# for i in range(11):
+#     print(f"{i} * {i} = {i*i}")
+
+
+# list= ['Python', 'Numpy','Pandas','Django', 'Flask']
+
+# for i in list:
+#     print(i)
+
+# even number 1-100
+
+# for i in range(100):
+#     if i % 2 == 0:
+#         print(i)
+
+# for i in list(range(100, -1, -1)):
+#     if i % 2 ==0:
+#         print(i)
+
+# for i in range(100, -1, -2):
+#     print(i)
+# m=0
+# for i in range(101):
+#     m+=i
+# print(m)
+
+# odd_num = 0
+# even_num = 0
+
+# for i in range(101):
+#     if i % 2==0:
+#         even_num+=i
+#     elif i % 2 !=0:
+#         odd_num+=i
+# print(f"Total even numbers sum {even_num}, and total odd numbers sum {odd_num}")
+
+
+# Prime number
+
+number = []
+
+# for n in range(2,100):
+#     prime = True
+#     for i in range(2, n+1):
+#         if n % i == 0:
+#             prime= False
+#             break
+#     if prime: 
+#         number.append(n)
+# print(number)
+
+
+# Tub = 1
+# n = 23
+# for i in range(2, n):
+#     if n % i == 0:
+#         Tub= 0
+# if Tub:
+#     print("True")
+# else:
+#     print("False")
+
+    
+        
+
+
