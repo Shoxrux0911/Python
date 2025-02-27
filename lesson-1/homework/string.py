3f0a81c1072220f6b82518604592fc10f7c7e2f4
Shoxrux0911 <usmonovvshohruhh@gmail.com>
2025-02-27 14:48:36 +0500

    Create string.py

---
+++ lesson-1/homework/string.py
@@ -0,0 +1,525 @@
+# x = (42)
+# i = int(x)
+# print(i)
+
+# n = 3.14
+# fl= float(n)
+# print(fl)
+
+# print(23//7)
+
+# print(29%3) 
+
+# print(4**4)
+
+# print(divmod(45, 8))
+
+# b = 3.6
+# c = 10**4
+# print(3.6e4)
+# print(b*c)
+
+# print(int('1101', 2)) ---binary String
+
+# print(int('2A',16))   ---hexademical string
+
+# x= 1_000_000_000
+
+# print(x)
+
+# n = input(("Enter yor age: " ))
+# print(int(n))
+
+# a = int(input("your age: "))
+# print("yur age: ", a)
+
+# s = float(input("Enter square number: "))
+# print(s**2)
+
+# x = 9
+# y= 14
+
+# print(abs(y-x))
+
+# my_float = 3.5
+# my_float+=1.2
+# print(my_float)
+
+# a = (3 + 4j)
+# b= (1-2j)
+# print(a*b)
+
+# r = float(input("enter circle radius: "))
+# pi= 3.14
+# print("radius: ", 2*pi*r)
+
+# print(pow(7, 4))
+
+# print("hello\\world")
+# print("hello\world")
+# print("hello\nworld")
+# print("hello\tworld")
+
+# name = "David"
+# # n = name[0]
+# # m = name[len(name)//2]
+# # f = name[-1]
+# # print(n+m+f)
+
+# print(name[0] + name[len(name)//2] + name[-1])
+
+# name = "David"
+
+# print(name[::-1])
+
+# s = "Hello, World"
+
+# print(s[7:12])
+
+# s = "Python Programming"
+# print(s[-5:])
+
+# s = "abcdefg"
+# print(s[::2])
+
+# s = "Hello, World!"
+# print(s[3:9])
+
+# s = "MAAB ACADEMY"
+# print(s[:4])
+
+# s = "Data Engineering"
+# print(s[:5])
+
+# s = "abcdefgh"
+# print(s[::3])
+
+
+# s = "Python Programming"
+
+# print(s[1::3])
+
+# s = "hello world, this is python"
+# print(s.title())
+
+
+# s = "Python"
+# print(s.center(20, '*'))
+
+
+# s = "Data Science"
+# print(s.ljust(20, '-'))
+
+# s = "Machine Learnin/
+
+# s = "Hello\tWorld"
+
+# print(s)
+
+
+# s = "Welcome"
+# print(s.center(15))
+
+# s= '42'
+
+# print(s.zfill(5))
+
+
+# s = " Python\tProgramming "
+
+# print(s.expandtabs(4))
+
+
+# s = " Python Programming "
+
+# print(s.strip())
+
+
+# s = " ***Data Science*** "
+# print(s.strip('_'))
+
+# s = "apple,banana,cherry"
+# print(s.split(','))
+
+
+# s = "John Doe 25 New York"
+# print(s.split())
+
+# s = "user@domain.com"
+# print(s.split('@'))
+
+# words = ["Python", "is", "fun"]
+# print('     '.join(words))
+
+
+# s = "PreprocessingData"
+# print(s.removeprefix('Pre'))
+
+# words = ["Python", "is", "fun"]
+# print(" ".join(words))
+
+# s = "filename.txt"
+# print(s.removesuffix('.txt'))
+
+# s = "one two three four"
+# print(s.rsplit(" ", 1))
+
+# s = "Hello\nWorld\nPython"
+# print(s.splitlines())
+
+# s = "HelloWorld"
+
+# print(s.isalpha())
+
+# s = " "
+# print(s.isspace())
+
+# s = "valid_identifier"
+# print(s.isidentifier())
+
+# s = "hello world, hello Python"
+# print(s.count('hello'))
+
+# s = "I love programming in Python"
+
+# print(s.find('prog'))
+
+# s = "Welcome to the world of Python"
+# print(s.index('Python'))
+
+
+# s = "I love cats, cats are great"
+# print(s.replace('cats', 'dogs'))
+
+
+# s = "look here, look there, look everywhere"
+# print(s.rfind("look"))
+
+# s = "apple banana apple grape"
+
+# print(s.rfind('apple'))
+
+# s = "Hello, World!"
+# print(s.startswith('Hello'))
+
+
+# s = "file_report.pdf"
+# print(s.endswith('pdf'))
+
+# s = "find me in the middle of the text"
+# print(s.find('middle', 5, 25))
+
+# s = "red red red red"
+# print(s.replace('red', 'blue', 2))
+
+# name = 'Alice'
+# age = 30
+# print("My name is {}, my age is {}".format(name, age))
+
+# name = 'Bob'
+# age = 25
+
+# print("my name is {} and {} years old ". format(name, age))
+
+# animal = 'dog'
+# print("domestic animal {}, and {} is kind". format(animal, animal))
+
+# first_name = 'John'
+# age = 40
+
+# print("my name is {0}, and {1} years old ".format(first_name, age))
+
+# text = 'Python'
+
+# # Format the string to center it with a width of 20 characters
+# formatted_text = "{:^20}".format(text)
+
+# # Print the resulting string
+# print(formatted_text)
+
+# info = {'name': 'Eve', 'age': 22}
+# print("Hello {name}, you are {age} years old".format(**info))
+
+# name = 'Mike'
+# age = 25
+
+# print("hello {}, and you are {} years old".format(name, age))
+
+# price = 100
+# tax_rate = 0.10
+
+# price_after_tax = price * (1+ tax_rate)
+
+# sentence = f"the price after tax is {price_after_tax}"
+# print(sentence)
+
+# name = 'Alice'
+# print(f"lower case {name}".lower())
+
+
+# age = 30
+# age+=5
+
+# print(f"your age in 5 years will be {age}")
+
+# name = "Bob"
+# print(f"my name is {name\n}")
+
+# username = 'jdoe'
+# print(f"your username is '{username}'")
+
+# temperature = 20
+# print(f"tempreture is {temperature}")
+
+# ------------------
+# 3 lesson
+
+# fruits = ['apple', 'banana', 'cherry']
+
+# emp_list1 = []
+# emp_list2 = list()
+
+# l1 = list('hello')
+
+# list1 = ([1, 2], [3, 4],  [5, 6])
+
+# print(list1[1])
+
+# colors = ['red', 'green', 'blue', 'yellow']
+
+# print("first element: ", colors[0] )
+# print("last element: ", colors[-1] )
+# print("second and third element: ", colors[1:3])
+
+
+# mixed = ['Python', 42, 3.14, [1, 2, 3]]
+# print(mixed)
+
+
+# num = [23,24,54,34] 
+# print(*num)
+
+# num = [23,24,54,34] 
+# count = len(num)
+
+# print(count)
+
+# list_num = range(1,10)
+
+# frst_el = list_num[0]
+# last_el = list_num[-1]
+# middle_el = list_num[len(list_num)//2]
+
+# print(frst_el)
+# print(last_el)
+# print(middle_el)
+
+# list_num = range(1,10)
+
+# first_three = list_num[:3]
+# last_three = list_num[-3:]
+# frst_last = list_num[1:-1]
+
+# print(list(first_three))
+# print(list(last_three))
+# print(list(frst_last))
+
+list_num = range(1,10)
+
+# evry_second = list_num[::2]
+# revers = list_num[::-1]
+
+# print(list(evry_second))
+# print(list(revers))
+
+
+list_num = list(range(1,10))
+
+# middle_three = list_num[len(list_num)//2]
+# midle = list_num[middle_three-1:middle_three+2] = [0, 0, 0]
+
+# print(list(midle))
+
+# list_num=list(list_num)
+# middle_in = list_num[len(list_num)//2]
+# list_num[middle_in-1:middle_in+2] = [0,0,0]
+# print(list_num)
+
+# rem_num = list_num[3:]
+
+# print(rem_num)
+
+# sum_num = sum(list_num)
+# print(sum_num)
+
+# max_num = max(list_num)
+# print(max_num)
+
+# min_num = min(list_num)
+# print(min_num)
+
+list_num = list(range(1,10)) 
+
+# copy_num = list_num.copy()
+
+# print(copy_num)
+
+# convert :str = list_num
+
+# print(convert)
+
+
+# list_num = [20, 40, 10, 80, 30, 50, 60]
+# # list_num.sort()
+
+
+# # print(list_num[1])
+
+# list_num.sort()
+
+# print(list_num[-2])
+
+
+list_num = [20, 40, 10, 80, 30, 50, 60]
+# list_num.append(2)
+# list_num.append(3)
+
+# list2 = 92, 81
+# list_num.append(list2)
+
+# print(list_num)
+
+
+
+# list_num = [20, 40, 10, 80, 30, 50, 60]
+
+# list_num.sort()
+
+# print(list_num)
+
+# list_num = [20, 40, 10, 80, 30, 50, 60]
+
+# list_num.sort(reverse=True)
+
+# print(list_num)
+
+# list_num = [20, 40, 10, 80, 30, 50, 60]
+
+# rem_list = list_num.pop(1)
+
+# print(list_num)
+# print(rem_list)
+
+# list_num = [20, 40, 10, 80, 30, 50, 60]
+# # remove_list = list_num.remove(20)
+
+# # print(list_num)
+
+# list_num.insert(1, 32)
+
+# print(list_num)
+
+
+# list_num = [20, 40, 10, 80, 30, 50, 60]
+
+# index_of = list_num.index(60)
+
+# print(index_of)
+
+# list_num = [4, 2, 1]
+# list2 = [10, 8, 6]
+
+# list_num.extend(list2)
+
+# print(list_num)
+
+# list_num = [3, 6, 3, 9, 3, 5, 7, 10, 12]
+
+# print(list_num.count(50))
+
+# list_num = [3, 6, 3, 9, 3, 5, 7, 10, 12]
+# list_num.clear()
+
+# print(list_num)
+
+
+# list_num = [3, 6, [7, 8, 9], 10, 12, 14]
+# list2 = list_num.copy()
+
+
+# print(list_num)
+# print(list2)
+
+# a = 10, 20, 30
+
+# print(type(a))
+
+# fruits = ('apple', 'banana', 'cherry')
+
+# print(fruits)
+
+
+# a = (42,)
+# print(type(a))
+
+# a = (      )
+# b = tuple(           )
+
+# print(len(a))
+# print(len(b))
+
+# list = ['a', 'b', 'c'] 
+# list2 = tuple(list)
+
+# print(list2)
+# print(tuple(list))
+
+
+# a = 20, 30, 40, 50, 60
+
+# print(a[1])
+
+# b = (3, 6, 9, 12)
+
+# print(b[-1])
+
+
+# a = (7, 1, 7, 3, 7, 4)
+
+# print(a.count(7))
+
+# a = (8, 4, 6, 4, 10)
+# print(a.index(4))
+
+# a = (1, 2)
+# b = (3, 4)
+
+# print(a + b)
+
+
+# a = (100, 200, 300, 400, 500)
+
+# print(len(a))
+
+# a = ('Python', 'SQL', 'HTML')
+
+# (cours2, course1, course) = a
+
+# print(course1)
+# print(course)
+# print(cours2)
+
+# a = (1, 2, 3, 4, 5)
+# (x, *y, z) = a
+
+# print(y)
+
+# a = ((1, 2), (3, 4), (5, 6))
+
+# print(a[2][0])
+
+
+
+
+
+
